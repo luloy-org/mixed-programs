35a893cddbe7f01e46c82351f15b2e324e7f371a
rem <86702826+reyyymartt@users.noreply.github.com>
2024-10-05 09:06:32 +0800

    Add files via upload

--- BMIcalcu.cxx
+++ BMIcalcu.cxx
@@ -6,24 +6,30 @@ using namespace std;
 
 int main(int argc, char *argv[])
 {
-    int weight;
-    double height;
-    double bmi;
-    string categ;
-    cout << "BMI Calculator\n";
-    cout << "Weight(kg): ";
-    cin >> weight;
-    cout << "Height(m): ";
-    cin >> height;
-    height = height * height;
-    bmi = weight / height;
-    cout << "Result: \n";
-    cout << "BMI: " << bmi << endl;
-
-    switch (true):
-	case (bmi<18.5):
-	cout<<"Underweight";
-    break;
-default:
-    cout << "An error occured, input the correct format\n";
+	int weight;
+	double height;
+	double bmi;
+	string categ;
+	cout << "BMI Calculator\n";
+	cout << "Weight(kg): ";
+	cin >> weight;
+	cout << "Height(m): ";
+	cin >> height;
+	height = height * height;
+	bmi = weight / height;
+	cout << "Result: \n";
+	cout<<"---------------"<<endl;
+	
+	if (bmi < 18.5){
+		categ="Underweight";
+	} else if (bmi == 18.5 or bmi < 24.9){
+		categ="Normal";
+	} else if (bmi == 25 or bmi < 29.9){
+		categ="Overweight";
+	} else if (bmi >= 30){
+		categ="Obese";
+	};
+	cout<<"BMI: "<<bmi<<endl;
+	cout<<"CATEGORY: "<<categ<<endl;
+	cout<<"---------------"<<endl;
 }
