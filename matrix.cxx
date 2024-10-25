d07bebe5b434ff2bed6fc7a0ffaa915bb263eed7
rem <reymartcenteno03@gmail.com>
2024-10-25 14:06:02 +0800

    Add files via upload

---
+++ matrix.cxx
@@ -0,0 +1,70 @@
+#include <iostream>
+#include <vector>
+#include <cstdlib>
+#include <ncurses.h>
+#include <utility>
+#include <string>
+
+using namespace std;
+const int c = 35;
+const int r = 35;
+const int onVal = 5;
+const int offVal = 0;
+bool rendering = true;
+
+vector<vector<int>> matrix(c,vector<int>(r, offVal));
+
+class square {
+    private:
+    vector<int> ps = {
+        0,//Column
+        0//Row
+        };
+    vector<int> sc = {1,1};
+    public:
+    bool fill = false;
+    void position (int x, int y){
+        ps = {x, y};
+    }
+    void scale (int x, int y){
+        sc = {x, y};
+    }
+    void render (){
+        
+        int leftBorder;
+        int rightBorder;
+        leftBorder=(ps[1]-sc[0]);
+        rightBorder=(ps[1]+sc[1]);
+        
+        
+        matrix[ps[0]][leftBorder]=onVal;
+        matrix[ps[0]][rightBorder]=onVal;
+        matrix[ps[0]][ps[1]]=onVal;
+    }
+};
+
+void resetMatrix (){
+    matrix = std::vector<std::vector<int>>(c, std::vector<int>(r, 0));
+}
+
+int main() {
+    square sq;
+
+    sq.position((c/2), (r/2));
+    sq.scale(2,2);
+    
+    // Display the initial matrix
+    while(rendering){
+        system("cls");
+        resetMatrix();
+        sq.render();
+        cout<<"Matrix: \n";
+    for (const auto& row : matrix) {
+        for (int elem : row) {
+            std::cout << elem << " ";
+        }
+        std::cout << std::endl;
+    }
+    }
+    return 0;
+}
